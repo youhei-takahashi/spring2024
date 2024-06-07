bc86ed58c7eb7d2a88e98cca1b366054cb7fb24c
youhei-takahashi <y.takahashi@morijyobi.ac.jp>
2024-06-07 14:49:11 +0900

    図書登録画面のダミーを作成

--- src/main/java/jp/ac/morijyobi/controller/BookController.java
+++ src/main/java/jp/ac/morijyobi/controller/BookController.java
@@ -1,2 +1,15 @@
-package jp.ac.morijyobi.controller;public class BookController {
+package jp.ac.morijyobi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @GetMapping("/register")
+    public String register() {
+        return "book/register";
+    }
 }
